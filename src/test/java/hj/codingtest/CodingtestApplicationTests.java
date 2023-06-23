14a5ebba9c078cf7400124d7153b8248bbb95285
PiHyoJeong <shson4007@gmail.com>
2023-06-23 17:04:49 +0900

    chore: 폴더 구조 변경

---
+++ src/test/java/hj/codingtest/CodingtestApplicationTests.java
@@ -0,0 +1,13 @@
+package hj.codingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
