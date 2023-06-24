4b9f6e6210a27605e9fc91a816466c5bf81d68fc
PiHyoJeong <shson4007@gmail.com>
2023-06-25 00:53:58 +0900

    Refactor: 변화량 적용하여 간단하게 구현

--- src/main/java/hj/codingtest/programmers/삼각_달팽이.java
+++ src/main/java/hj/codingtest/programmers/삼각_달팽이.java
@@ -1,65 +1,43 @@
 package hj.codingtest.programmers;
 
 public class 삼각_달팽이 {
-    public int[] solution(int n) {
 
-        if (n == 1) {
-            return new int[]{1};
-        }
+    private static final int[] dx = {0, 1, -1};
+    private static final int[] dy = {1, 0, -1};
+
+    public int[] solution(int n) {
 
         int[][] triangle = new int[n][n];
 
         int x = 0;
         int y = 0;
-        int v = 1;
+        int number = 1;
+        int direction = 0;
+        // 0: 아래, 1: 오른쪽, 2: 왼쪽 위
 
         while (true) {
 
-            //아래
-            while (true) {
-                triangle[y][x] = v++;
-                if (y + 1 == n || triangle[y + 1][x] != 0) {
+            triangle[y][x] = number++;
+
+            int nx = x + dx[direction];
+            int ny = y + dy[direction];
+
+            if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
+
+                direction = (direction + 1) % 3;
+
+                nx = x + dx[direction];
+                ny = y + dy[direction];
+
+                if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
                     break;
                 }
-                y += 1;
             }
-            if (triangle[y][x + 1] != 0 || x + 1 == n) {
-                break;
-            }
-            x += 1;
-
-
-            //오른쪽
-            while (true) {
-                triangle[y][x] = v++;
-                if (x + 1 == n || triangle[y][x + 1] != 0) {
-                    break;
-                }
-                x += 1;
-            }
-            if (triangle[y - 1][x - 1] != 0) {
-                break;
-            }
-            x -= 1;
-            y -= 1;
-
-            //왼쪽 위
-            while (true) {
-                triangle[y][x] = v++;
-                if (triangle[y - 1][x - 1] != 0) {
-                    break;
-                }
-                x -= 1;
-                y -= 1;
-            }
-            if (y + 1 == n || triangle[y + 1][x] != 0) {
-                break;
-            }
-            y += 1;
-
+            x = nx;
+            y = ny;
         }
 
-        int[] answer = new int[v - 1];
+        int[] answer = new int[number - 1];
         int index = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
--- src/test/java/hj/codingtest/programmers/삼각_달팽이_test.java
+++ src/test/java/hj/codingtest/programmers/삼각_달팽이_test.java
@@ -1,11 +1,9 @@
 package hj.codingtest.programmers;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
